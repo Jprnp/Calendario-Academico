2732923ac420915ec672a32221c9b3f922baf253
Jprnp <jprnp18@gmail.com>
2016-11-24 10:15:06 -0200

    Update App.java

--- CalendarioAcademico/src/main/java/com/github/jprnp/Calendario_Academico/calendarioacademico/App.java
+++ CalendarioAcademico/src/main/java/com/github/jprnp/Calendario_Academico/calendarioacademico/App.java
@@ -333,7 +333,6 @@ public class App {
 
         boolean done = false;
         do {
-            leitor = null;
             dt = leitor.nextLine();
             if (dt.matches(regex)) {
                 done = true;
